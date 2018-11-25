712a4156a1b6f5e9db941b721fa4f806f8998f3a
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-11-25 09:23:43 +0700

    Membuat class Transaksi untuk proses pembayaran pembelian item dan membuat variablenya

---
+++ Source Packages/Falah/Coffee/Transaksi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Falah.Coffee;
+
+/**
+ *
+ * @author MSI Notebook
+ */
+public class Transaksi {
+    
+    // variable code untuk menampilkan kode pembayaran minuman
+    private final String kode ;
+    // variable item untuk menampilkan item apa saja yang dibeli
+    private ArrayList<Item> items = new ArrayList<>() ;
+    // variable total untuk menampilkan total pembelian minuman
+    private float total ; 
+    
+    
+}
