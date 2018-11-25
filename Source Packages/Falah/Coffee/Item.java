fc983e75c37f5117f93641f0e122139fdea7bf8f
MSI Notebook <MSI Notebook@DESKTOP-2D6BNE3>
2018-11-25 09:05:08 +0700

    membuat class Item dan membuat variable

---
+++ Source Packages/Falah/Coffee/Item.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Falah.Coffee;
+
+/**
+ *
+ * @author MSI Notebook
+ */
+public class Item {
+    // variable ini untuk menyimpan nama barang
+    private final String nama ;
+    // variable ini untuk menyimpan harga barang
+    private float harga ; 
+    // dan terakhir variable ini untuk menyimpan jumlah barang
+    private int jumlah ; 
+    
+    
+        
+    }
+
